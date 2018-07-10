e3fa09cb832dd370a940c302f7906051d07e70e4
John Waymire <waymire68@yahoo.com>
2018-07-09 20:38:47 -0400

    added components, reversed index/AddExpense files

--- client/src/pages/AddExpense/AddExpense.js
+++ client/src/pages/AddExpense/AddExpense.js
@@ -1,1 +1,18 @@
-export { default } from './AddExpense';
+import React, { Component } from 'react';
+import ExpenseInput from '../../components/ExpenseInput/ExpenseInput';
+
+class AddExpense extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <h2 className="text-center">Add an Expense</h2>
+            <ExpenseInput />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default AddExpense;
