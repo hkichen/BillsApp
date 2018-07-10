43b23c89e57cf3194fcbfc77c02b3f185fc890d2
John Waymire <waymire68@yahoo.com>
2018-07-10 19:19:59 -0400

    routes, controllers

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/api/users');
+var expense = require('./routes/api/expense');
 
 var app = express();
 
@@ -17,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api/users', users);
+app.use('/api/expense', expense);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,7 +35,7 @@ app.use(function(err, req, res, next) {
 
   // send error json
   res.status(err.status || 500);
-  res.json({message: "Uh-oh, error."});
+  res.json({ message: 'Uh-oh, error.' });
 });
 
 module.exports = app;
---
+++ controllers/expenseController.js
@@ -0,0 +1,24 @@
+const Expense = require('../models').Expense;
+
+module.exports = {
+  update: function(req, res) {
+    Expense.update(req.body)
+      .then(dbExpense => res.json(dbExpense))
+      .catch(err => res.status(422).json(err));
+  },
+  findAll: function(req, res) {
+    Expense.findAll({})
+      .then(dbExpense => res.json(dbExpense))
+      .catch(err => res.status(422).json(err));
+  },
+
+  create: function(req, res) {
+    Expense.create({
+      category: req.body.category,
+      avgAmount: req.body.avgAmount,
+      description: req.body.description
+    })
+      .then(dbExpense => res.json(dbExpense))
+      .catch(err => res.status(422).json(err));
+  }
+};
