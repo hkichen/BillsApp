4e5a8fc2860177e467654700c2bd807dea31dea3
unknown <hkichen@gmail.com>
2018-07-10 21:33:58 -0400

    change app to router in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+ 
 
 //home api route
-app.get("/*", (req, res, next) => {
+router.get("/*", (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"))
 })
 
