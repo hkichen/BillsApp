61aec9a3224e2ae31b6e25408417abc1642098bf
Ryan McKenzie <ryanjm88@gmail.com>
2018-07-10 21:25:46 -0400

    navbar thangz

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,14 +4,9 @@ import Landing from '../../pages/Landing';
 import Users from '../users/users';
 import User from '../users/user';
 import Dashboard from '../../pages/Dashboard';
-<<<<<<< HEAD
 import AddExpense from '../../pages/AddExpense';
 import Statement from '../../components/Statement';
 import StatementInfo from '../../components/StatementInfo';
-=======
-import Signup from '../../pages/Signup';
-
->>>>>>> 57c7786460969f6a8dc54e3c99f968a6af10bba9
 
 class Navbar extends Component {
   render() {
@@ -19,7 +14,6 @@ class Navbar extends Component {
       <Router>
         <div className="navbar">
           <ul>
-<<<<<<< HEAD
             <li>
               <Link to="/"> Landing </Link>
             </li>
@@ -42,21 +36,6 @@ class Navbar extends Component {
           <Route exact path="/users" component={Users} />
           <Route exact path="/users/:id" component={User} />
           <Route exact path="/statements" component={Statement} />
-=======
-            <li><Link to="/"> Landing </Link></li>
-            <li><Link to="/dashboard"> Dashboard </Link></li>
-            <li><Link to="/users"> Users </Link></li>
-            <li><Link to="/signup"> Sign Up </Link></li>
-          </ul>
-  
-          <hr/>
-  
-          <Route exact path="/" component={Landing}/>
-          <Route exact path="/dashboard" component={Dashboard}/>
-          <Route exact path="/users" component={Users}/>
-          <Route exact path="/users/:id" component={User}/>
-          <Route exact path="/signup" component={Signup} />
->>>>>>> 57c7786460969f6a8dc54e3c99f968a6af10bba9
         </div>
       </Router>
     );
