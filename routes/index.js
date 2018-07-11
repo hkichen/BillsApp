dafbedeed12867d1cbcecee93b744101dcf47a99
unknown <hkichen@gmail.com>
2018-07-10 21:30:03 -0400

    changed home route to send index.html

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
+
 
 //home api route
-app.get("/", (req, res, next) => {
+app.get("/*", (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"))
-});
+})
+
 
 module.exports = router;
