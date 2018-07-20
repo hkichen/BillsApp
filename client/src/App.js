d98817604260e5ebbea605551481acc065f51732
Lina Kichen <hkichen@gmail.com>
2018-07-20 15:23:57 -0400

    Add authentication to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import './App.css'
 import Landing from './pages/Landing';
 import Dashboard from './pages/Dashboard';
 import AddExpense from './pages/AddExpense';
 import Navbar from './components/Navbar';
-
-import Footer from './components/Footer';
-
+//  import Footer from './components/Footer';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import ProfileForm from './pages/ProfileForm';
+import Auth from './Auth/Auth';
+import history from './history';
+import Callback from './components/Callback/Callback';
 
 
-const App = () => (
-  <Router>
+const auth = new Auth();
+
+const handleAuthentication = (nextState, replace) => {
+  if (/access_token|id_token|error/.test(nextState.location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
+export const AppRouter = () => {
+  
+  return (
+    <Router history={history} component={Landing}>
     <div>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addexpense" component={AddExpense} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/profileform" component={ProfileForm} />
-        <Route exact path="/signup" component={Signup} />
-        {/* <Route exact path="/users" component={Users} />
-        <Route exact path="/users/:id" component={User} /> */}
-      </Switch>
-      <Footer />
+    {/* <Navbar auth={auth} /> */}
+    <Route path="/" render={(props) => <Navbar auth={auth} {...props} />} />
+    <Route path="/landing" render={(props) => <Landing auth={auth} {...props} />} />
+    <Route path="/dashboard" render={(props) => <Dashboard auth={auth} {...props} />} />
+    <Route path="/addexpense" render={(props) => <AddExpense auth={auth} {...props} />} />
+    <Route path="/profile" render={(props) => <Profile auth={auth} {...props} />} />
+    <Route path="/profileform" render={(props) => <ProfileForm auth={auth} {...props} />} />
+    <Route path="/signup" component={Signup} />
+    <Route path="/callback" render={(props) => {
+      handleAuthentication(props);
+      return <Callback {...props} /> 
+    }}/>
     </div>
-  </Router>
-);
+    </Router>
+  );
+}
 
-export default App;
