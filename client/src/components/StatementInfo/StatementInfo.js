67ba74d6c7979a7b8d5a0a4744a07c4e5a9329de
John Waymire <waymire68@yahoo.com>
2018-07-22 19:27:38 -0400

    forgot statementinfo page

--- client/src/components/StatementInfo/StatementInfo.js
+++ client/src/components/StatementInfo/StatementInfo.js
@@ -8,18 +8,15 @@ class StatementInfo extends Component {
     this.state = {};
 
     this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(event) {
     const target = event.target;
     const value = target.type === 'select' ? target.selected : target.value;
     const name = target.name;
-    console.log(target);
 
     this.setState({
       [name]: value
     });
-    console.log(this.state);
   }
 
   render() {
