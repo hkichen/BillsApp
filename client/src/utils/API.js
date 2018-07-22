6c34849a5ee25d4cb52ee44d853f9f71f3adbc0b
Lina Kichen <hkichen@gmail.com>
2018-07-22 11:14:01 -0400

    changes api call to get and patch user metadata by sending request to backend server to handle

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,22 +1,50 @@
 import axios from 'axios';
 
 export default {
-  //Get all users
-  getUsers: function() {
-    return axios.get('/api/users');
+  // //Get all users
+  // getUsers: function() {
+  //   return axios.get('/api/users');
+  // },
+  // // Get a user by id
+  // getUSer: function(id) {
+  //   return axios.get('/api/users/' + id);
+  // },
+  // // Deletes user by id
+  // deleteUser: function(id) {
+  //   return axios.delete('/api/users/' + id);
+  // },
+  // // adds new user to the database
+  // createUser: function(userData) {
+  //   return axios.post('/api/users', userData);
+  // },
+  getMeta: function(sub) { 
+    const url = '/api/users/' + sub;
+    return axios({
+      method: 'get',
+      url: url,
+      headers: {
+        authorization:`Bearer ${localStorage.getItem('access_token')}`
+      }
+    })
   },
-  // Get a user by id
-  getUSer: function(id) {
-    return axios.get('/api/users/' + id);
+
+  updateMeta: function(data) {
+    const url = '/api/users/' + data.sub;
+    return axios({
+      method: 'post',
+      url: url,
+      data: {
+        lastName: data.lastName,
+        firstName: data.firstName,
+        monthlyIncome: data.monthlyIncome
+      },
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+      }
+    })
   },
-  // Deletes user by id
-  deleteUser: function(id) {
-    return axios.delete('/api/users/' + id);
-  },
-  // adds new user to the database
-  createUser: function(userData) {
-    return axios.post('/api/users', userData);
-  },
+
+
   //updates new Expense to database
   createExpense: function(expenseData) {
     return axios.put('/api/expense/', expenseData);
