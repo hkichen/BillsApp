72bfe0695a6171ee9dfdc02e581688b5379eb13a
Lina Kichen <hkichen@gmail.com>
2018-07-22 11:12:17 -0400

    grab user's metadata and display to profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Profile.css';
+import API from '../../utils/API';
+
+
 
 class Profile extends Component {
+
+
+
   componentWillMount() {
     this.setState({ profile: {} });
     const { userProfile, getProfile } = this.props.auth;
     if (!userProfile) {
       getProfile((err, profile) => {
         this.setState({ profile });
+        console.log(profile)
+
+        const subArray = profile.sub.split("|");
+        const sub = subArray[0]+"%7C"+subArray[1];
+
+        API.getMeta(sub)
+          .then(res => {
+            console.log(res)
+            console.log("metadata: ")
+            console.log(res.data.user_metadata)
+            this.setState({metadata: res.data.user_metadata || {}})
+          })
+          .catch(err => console.log(err))
       });
     } else {
       this.setState({ profile: userProfile });
@@ -19,6 +38,7 @@ class Profile extends Component {
     
     const { profile } = this.state;
     const { isAuthenticated } = this.props.auth;
+    
     return (
       isAuthenticated() ?
       <div className="container">
@@ -30,6 +50,7 @@ class Profile extends Component {
               <h4>Name: {profile.name} </h4>
             </div>
           </div>
+          <div>{JSON.stringify(this.state.metadata)}</div>
         </div>
         <button className="btn-warning"><Link to="/profileform">Update Profile</Link></button>
       </div>
