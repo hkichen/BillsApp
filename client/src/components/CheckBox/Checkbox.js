38a269e533429d06537db5c548cd3b61e567af55
John Waymire <waymire68@yahoo.com>
2018-07-22 19:08:21 -0400

    remove form-control from checkbox

--- client/src/components/CheckBox/Checkbox.js
+++ client/src/components/CheckBox/Checkbox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Checkbox = props => {
   return (
-    <div className="form-check form-control">
+    <div className="form-check">
       <input className="form-check-input" {...props} />
       <label className="form-check-label">Auto Pay</label>
     </div>
