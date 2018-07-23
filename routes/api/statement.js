9936a5b6d1fe91b1d88c9ef6a5c91d4f7c3f360a
Lina Kichen <hkichen@gmail.com>
2018-07-23 19:50:49 -0400

    create statement routes that take in user and expense id parameters

--- routes/api/statement.js
+++ routes/api/statement.js
@@ -7,4 +7,9 @@ const statementController = require('../../controllers').statement;
 router.get('/', statementController.findAll);
 router.post('/', statementController.create);
 
+//create a new statement that has a userId and ExpenseId
+router.get('/:userId/:expenseId', statementController.findWithIds);
+
+//create a new statement with userId and ExpenseId
+router.post('/:userId/:expenseId', statementController.createWithIds);
 module.exports = router;
