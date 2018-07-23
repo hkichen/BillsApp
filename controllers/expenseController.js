50fb4df0b4c80aec7ffcef4e3fef02662aa77d84
Lina Kichen <hkichen@gmail.com>
2018-07-23 13:48:19 -0400

    add method to find all by userId and bulk create

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -20,5 +20,85 @@ module.exports = {
     })
       .then(dbExpense => res.json(dbExpense))
       .catch(err => res.status(422).json(err));
+  },
+
+  findByUserID: function(req, res) {
+    Expense.findall({
+      where: {
+        userId: "google-oauth2%7C117865706151901461462"
+      }
+    })
+      .then(dbExpense => res.json(dbExpense))
+      .catch(err => res.send(err));
+  },
+  
+  bulkCreatewithID: function(req, res) {
+    Expense.bulkCreate([{
+      category: 'Rent/Mortgage',
+      avgAmount: 1200.00,
+      description: 'Suntrust Bank, Piedment Condo',
+      userId: 'google-oauth2%7C117865706151901461462',
+      createdAt:'2018-07-15',
+      updatedAt:'2018-07-15'
+    },
+    {
+      category: 'Utilities',
+      avgAmount: 200.00,
+      description: 'Georgie Power, Comcast',
+      userId: 'google-oauth2%7C117865706151901461462',
+      createdAt:'2018-07-15',
+      updatedAt:'2018-07-15'
+    },
+    {
+      category: 'Car/Transportation',
+      avgAmount: 240.00,
+      description: '2018 Subaru Forester, car payment',
+      userId: 'google-oauth2%7C117865706151901461462',
+      createdAt:'2018-07-15',
+      updatedAt:'2018-07-15'
+    },
+    {
+      category: 'Food/Dining',
+      avgAmount: 400.00,
+      description: 'Work lunches and groceries',
+      userId: 'google-oauth2%7C117865706151901461462',
+      createdAt:'2018-07-15',
+      updatedAt:'2018-07-15'
+    },
+    {
+      category: 'Credit Card',
+      avgAmount: 300.00,
+      description: 'VISA, AMEX',
+      userId: 'google-oauth2%7C117865706151901461462',
+      createdAt:'2018-07-15',
+      updatedAt:'2018-07-15'
+    },
+    {
+      category: 'Loans',
+      avgAmount: 120.00,
+      description: 'School loan',
+      userId: 'google-oauth2%7C117865706151901461462',
+      createdAt:'2018-07-15',
+      updatedAt:'2018-07-15'
+    },
+    {
+      category: 'Medical/Health',
+      avgAmount: 400.00,
+      description: 'Insurance premiums',
+      userId: 'google-oauth2%7C117865706151901461462',
+      createdAt:'2018-07-15',
+      updatedAt:'2018-07-15'
+    },
+    {
+      category: 'Other',
+      avgAmount: 300.00,
+      description: 'Cell phone, Gym membership',
+      userId: 'google-oauth2%7C117865706151901461462',
+      createdAt:'2018-07-15',
+      updatedAt:'2018-07-15'
+    },
+  ])
+  .then(dbExpenses => res.json(dbExpenses))
+  .catch(err => res.json(err))
   }
 };
