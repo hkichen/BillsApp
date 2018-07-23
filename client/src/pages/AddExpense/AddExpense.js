33b56398c4ea9efca6e06bf1fb38fd4c6e1c91fc
John Waymire <waymire68@yahoo.com>
2018-07-23 18:34:53 -0400

    changed margin-bottom to 120px

--- client/src/pages/AddExpense/AddExpense.js
+++ client/src/pages/AddExpense/AddExpense.js
@@ -6,21 +6,19 @@ class AddExpense extends Component {
   render() {
     const { isAuthenticated } = this.props.auth;
     return isAuthenticated() ? (
-      <div className="bg-img">
-        <div className="container">
-          <br />
-          <div className="row">
-            <div className="col-sm-6 offset-sm-3">
-              <h2 className="text-center">Update Expenses</h2>
-            </div>
+      <div className="container">
+        <br />
+        <div className="row">
+          <div className="col-sm-6 offset-sm-3">
+            <h2 className="text-center">Update Expenses</h2>
           </div>
-          <br />
-          <div className="row">
-            <div className="col-sm-8 offset-sm-2">
-              <div className="card">
-                <div className="card-body" id="expense-input">
-                  <ExpenseInput />
-                </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-sm-8 offset-sm-2">
+            <div className="card">
+              <div className="card-body" id="expense-input">
+                <ExpenseInput />
               </div>
             </div>
           </div>
