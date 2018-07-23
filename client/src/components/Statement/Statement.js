b0bb8baf90c11aea2de3ae338a47729e4379a943
John Waymire <waymire68@yahoo.com>
2018-07-23 17:58:34 -0400

    fixed scroll on statement

--- client/src/components/Statement/Statement.js
+++ client/src/components/Statement/Statement.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import CustomSelect from '../CustomSelect';
 import Checkbox from '../CheckBox';
 import API from '../../utils/API';
+import './Statement.css';
 
 class Statement extends Component {
   constructor(props) {
@@ -34,77 +35,75 @@ class Statement extends Component {
     const { isAuthenticated } = this.props.auth;
 
     return isAuthenticated() ? (
-      <div className="bg-img">
-        <div className="container">
-          <br />
-          <div className="row">
-            <div className="col-sm-6 offset-sm-3">
-              <h2 className="text-center">Add A Bill Statement</h2>
-            </div>
+      <div className="container">
+        <br />
+        <div className="row">
+          <div className="col-sm-6 offset-sm-3">
+            <h2 className="text-center">Add A Bill Statement</h2>
           </div>
-          <br />
-          <div className="row">
-            <div className="col-sm-8 offset-sm-2">
-              <div className="card">
-                <div className="card-body" id="statement-input">
-                  <form onSubmit={this.handleSubmit}>
-                    <CustomSelect
-                      name="id"
-                      value={this.state.id}
-                      onChange={this.handleChange}
-                    />
-                    <label>Bill Name</label>
-                    <input
-                      className="form-control"
-                      name="description"
-                      type="text"
-                      value={this.state.description}
-                      onChange={this.handleChange}
-                      placeholder="Visa, Power Company..."
-                    />
-                    <label>Bill Amount</label>
-                    <input
-                      className="form-control"
-                      name="avgAmount"
-                      type="text"
-                      value={this.state.avgAmount}
-                      onChange={this.handleChange}
-                      placeholder="00.00"
-                    />
-                    <label>Paid From Account</label>
-                    <input
-                      className="form-control"
-                      name="description"
-                      type="text"
-                      value={this.state.moneySource}
-                      onChange={this.handleChange}
-                      placeholder="Bank/Credit Card"
-                    />
-                    <Checkbox
-                      id="auto-pay"
-                      type="checkbox"
-                      value={this.state.autoPay}
-                      onChange={this.handleChange}
-                    />
-                    <br />
-                    <label>Upload Image or File</label>
-                    <input
-                      className="form-control"
-                      id="statementUpload"
-                      type="file"
-                      accept="image/*;capture=camera"
-                      value={this.state.photo}
-                      onChange={this.handleChange}
-                    />
-                    <br />
-                    <br />
-                    <input
-                      className="btn btn-warning btn-lg"
-                      type="submit"
-                      value="Submit"
-                    />
-                  </form>
-                </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-sm-8 offset-sm-2">
+            <div className="card">
+              <div className="card-body" id="statement-input">
+                <form onSubmit={this.handleSubmit}>
+                  <CustomSelect
+                    name="id"
+                    value={this.state.id}
+                    onChange={this.handleChange}
+                  />
+                  <label>Bill Name</label>
+                  <input
+                    className="form-control"
+                    name="description"
+                    type="text"
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                    placeholder="Visa, Power Company..."
+                  />
+                  <label>Bill Amount</label>
+                  <input
+                    className="form-control"
+                    name="avgAmount"
+                    type="text"
+                    value={this.state.avgAmount}
+                    onChange={this.handleChange}
+                    placeholder="00.00"
+                  />
+                  <label>Paid From Account</label>
+                  <input
+                    className="form-control"
+                    name="description"
+                    type="text"
+                    value={this.state.moneySource}
+                    onChange={this.handleChange}
+                    placeholder="Bank/Credit Card"
+                  />
+                  <Checkbox
+                    id="auto-pay"
+                    type="checkbox"
+                    value={this.state.autoPay}
+                    onChange={this.handleChange}
+                  />
+                  <br />
+                  <label>Upload Image or File</label>
+                  <input
+                    className="form-control"
+                    id="statementUpload"
+                    type="file"
+                    accept="image/*;capture=camera"
+                    value={this.state.photo}
+                    onChange={this.handleChange}
+                  />
+                  <br />
+                  <br />
+                  <input
+                    className="btn btn-warning btn-lg"
+                    type="submit"
+                    value="Submit"
+                  />
+                </form>
               </div>
             </div>
           </div>
