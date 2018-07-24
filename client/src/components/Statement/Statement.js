8620d98c499a198860d7003ea7d704e061fdb016
Lina Kichen <hkichen@gmail.com>
2018-07-23 22:37:40 -0400

    can now create and submit and new statement to db

--- client/src/components/Statement/Statement.js
+++ client/src/components/Statement/Statement.js
@@ -5,7 +5,67 @@ import Checkbox from '../CheckBox';
 import API from '../../utils/API';
 
 class Statement extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      description: '',
+      amount: '',
+      moneySource:'',
+      autoPay: false,
+      photo: '',
+      expenses: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    API.getExpenses()
+    .then(res => {
+      if (res.data.length === 0) {
+        API.createBulkExpenses()
+          //here we run a recursion by running the getExpense method again, but it will go to the else
+          //part because res.data.length is no longer 0
+          .then(res => this.setState({expenses: res.data}))
+          .catch(err => res.send(err))
+      } else {
+        console.log(res.data)
+        this.setState({
+          id: res.data[0].id,
+          expenses: res.data
+        });
+      }
+    })
+  }  
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    const expense = this.state.expenses.find((expense) => parseInt(expense.id, 10) === parseInt(this.state.id, 10));
+    const data = {
+      id: this.state.id,
+      category: expense.category,
+      description: this.state.description,
+      amount: this.state.amount,
+      moneySource: this.state.moneySource,
+      autoPay: this.state.autoPay,
+      photo: this.state.photo,
+      expenses: this.state.expenses
+    }
+    API.createStmtWithID(this.state.id, data)
+      .then(console.log("created new statement!"))
+  }
+
   render() {
     const { isAuthenticated } = this.props.auth;
     
@@ -25,6 +85,7 @@ class Statement extends Component {
                 <form onSubmit={this.handleSubmit}>
                   <CustomSelect
                     name="id"
+                    data={this.state.expenses}
                     value={this.state.id}
                     onChange={this.handleChange}
                   />
@@ -40,16 +101,16 @@ class Statement extends Component {
                   <label>Statement Amount</label>
                   <input
                     className="form-control"
-                    name="avgAmount"
-                    type="text"
-                    value={this.state.avgAmount}
+                    name="amount"
+                    type="number"
+                    value={this.state.amount}
                     onChange={this.handleChange}
                     placeholder="00.00"
                   />
                   <label>Paid From Account</label>
                   <input
                     className="form-control"
-                    name="description"
+                    name="moneySource"
                     type="text"
                     value={this.state.moneySource}
                     onChange={this.handleChange}
@@ -65,6 +126,7 @@ class Statement extends Component {
                   <label>Upload Image or File</label>
                   <input
                     className="form-control"
+                    name="photo"
                     id="statementUpload"
                     type="file"
                     accept="image/*;capture=camera"
