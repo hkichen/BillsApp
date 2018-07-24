3a9cf71f529213a41bd2d3453d67d9eefaa484fc
Lina Kichen <hkichen@gmail.com>
2018-07-24 16:15:39 -0400

    little path

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/api/expense', expense);
 app.use('/api/users', users);
 app.use('/api/statements', statements);
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"))
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
 
 // catch 404 and forward to error handler
