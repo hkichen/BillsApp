495272b117a7ede02125ee945ee3e3fd48242a10
Lina Kichen <hkichen@gmail.com>
2018-07-24 15:12:44 -0400

    change callback url to env

--- client/src/Auth/Auth-variables.js
+++ client/src/Auth/Auth-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     domain: 'billsapp.auth0.com',
     clientId: 'VhXISyrKYyVcIO4GnWVOwfy5k21UEYqG',
-    callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: process.env.URL + '/callback'
   }
   
