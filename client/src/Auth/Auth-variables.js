d2e6ee6926b38db0d6d1320f3b9d2cbd2e028eac
Lina Kichen <hkichen@gmail.com>
2018-07-24 15:28:04 -0400

    reconfig react env url

--- client/src/Auth/Auth-variables.js
+++ client/src/Auth/Auth-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-    domain: 'billsapp.auth0.com',
-    clientId: 'VhXISyrKYyVcIO4GnWVOwfy5k21UEYqG',
-    callbackUrl: process.env.URL + '/callback'
-  }
+  domain: 'billsapp.auth0.com',
+  clientId: 'VhXISyrKYyVcIO4GnWVOwfy5k21UEYqG',
+  callbackUrl: process.env.REACT_APP_AUTH_URL + '/callback'
+}
   
