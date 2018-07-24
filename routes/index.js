feada392848a6e84183b3155c2c7c85691cf3210
Lina Kichen <hkichen@gmail.com>
2018-07-24 16:08:09 -0400

    use catchalls

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,8 @@ var router = express.Router();
  
 
 //home api route
-router.get("/*", (req, res, next) => {
+router.get("*", (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"))
 })
-
 
 module.exports = router;
