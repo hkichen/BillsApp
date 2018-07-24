f6d23d02985d776507d6b4e5912664f537697718
Lina Kichen <hkichen@gmail.com>
2018-07-23 21:51:35 -0400

    remove functions from statement components

--- client/src/components/Statement/Statement.js
+++ client/src/components/Statement/Statement.js
@@ -5,31 +5,7 @@ import Checkbox from '../CheckBox';
 import API from '../../utils/API';
 
 class Statement extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  }
-  handleSubmit(event) {
-    event.preventDefault();
-
-    API.addStatements()
-      .then(res => {})
-      .catch(err => console.log(err));
-  }
-
+  
   render() {
     const { isAuthenticated } = this.props.auth;
     
