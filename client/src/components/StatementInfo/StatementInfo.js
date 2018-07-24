ea03d6d8cf838cbaf09aee5c223facc1bd0b765d
Lina Kichen <hkichen@gmail.com>
2018-07-23 23:52:12 -0400

    display all statements by userId, filter by expenseId

--- client/src/components/StatementInfo/StatementInfo.js
+++ client/src/components/StatementInfo/StatementInfo.js
@@ -1,23 +1,76 @@
 import React from 'react';
 import { Component } from 'react';
-import CustomSelect from '../CustomSelect';
+import API from '../../utils/API';
+
+const StatementSelect = props => {
+  return (
+    <div className="form-group">
+      <label>Expense Category</label>
+      <select className="custom-select" {...props}>
+        <option value="all">View All</option>
+        {props.data.map(function(expense) {
+          return <option key = {expense.id} value={expense.id}>{expense.category}</option>
+        })}
+      </select>
+    </div>
+  );
+};
 
 class StatementInfo extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      id: 'all',
+      statements: [],
+      activeStatements: [],
+      expenses: []
+    };
 
     this.handleChange = this.handleChange.bind(this);
   }
+
+  componentDidMount() {
+    API.getStatements()
+      .then(res => {
+        this.setState({
+          statements: res.data,
+          activeStatements: res.data
+        })
+      })
+    API.getExpenses()
+      .then(res => {
+        if (res.data.length === 0) {
+          API.createBulkExpenses()
+            //here we run a recursion by running the getExpense method again, but it will go to the else
+            //part because res.data.length is no longer 0
+            .then(res => this.setState({expenses: res.data}))
+            .catch(err => res.send(err))
+        } else {
+          this.setState({
+            expenses: res.data
+          });
+        }
+      })
+  }
+
   handleChange(event) {
     const target = event.target;
-    const value = target.type === 'select' ? target.selected : target.value;
+    const value = target.value;
     const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
+    if (value === "all") {
+      this.setState({
+        [name]: value,
+        activeStatements: this.state.statements
+      });
+    } else {
+      const activeStatements = this.state.statements.filter(statement => parseInt(statement.ExpenseId, 10) === parseInt(value, 10));
+      this.setState({
+        [name]: value,
+        activeStatements: activeStatements
+      });
+    }
   }
+
 
   render() {
     const { isAuthenticated } = this.props.auth;
@@ -35,8 +88,9 @@ class StatementInfo extends Component {
             <div className="card">
               <div className="card-body" id="statement-select">
                 <form onSubmit={this.handleSubmit}>
-                  <CustomSelect
+                  <StatementSelect
                     name="id"
+                    data={this.state.expenses}
                     value={this.state.id}
                     onChange={this.handleChange}
                   />
@@ -61,7 +115,7 @@ class StatementInfo extends Component {
                     <thead>
                       <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Bill Name</th>
+                        <th scope="col">Statement Description</th>
                         <th scope="col">Amount</th>
                         <th scope="col">Paid From</th>
                         <th scope="col">Auto Pay</th>
@@ -69,30 +123,18 @@ class StatementInfo extends Component {
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <th scope="row">1</th>
-                        <td>Visa/Capital One</td>
-                        <td>100.50</td>
-                        <td>Suntrust Checking</td>
-                        <td>Yes</td>
-                        <td>No</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">2</th>
-                        <td>Discover</td>
-                        <td>59.00</td>
-                        <td>Suntrust Checking</td>
-                        <td>Yes</td>
-                        <td>No</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">3</th>
-                        <td>Amex</td>
-                        <td>279.00</td>
-                        <td>Delta Credit Union</td>
-                        <td>No</td>
-                        <td>Yes</td>
-                      </tr>
+                      {this.state.activeStatements.map(function(statement, index) {
+                        return (
+                          <tr key={statement.id}>
+                            <th scope="row">{index + 1}</th>
+                            <td>{statement.description}</td>
+                            <td>{statement.amount}</td>
+                            <td>{statement.moneySource}</td>
+                            <td>{statement.autoPay ? "Yes" : "No"}</td>
+                            <td>{statement.photo ? "Yes" : "No"}</td>
+                          </tr>
+                        )
+                      })}
                     </tbody>
                   </table>
                 </div>
